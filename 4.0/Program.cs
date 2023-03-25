fe0ab766d7d448c986e0ad00ec6641caa5f2b1e7
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-25 15:15:12 +0500

    Add project files.

---
+++ 4.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double number;
+
+        while (true)
+        {
+            Console.Write("Enter a number : ");
+
+            if (double.TryParse(Console.ReadLine(), out number))
+            {
+                if (number % 1 == 0 || number % 1 != 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter an integer.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+
+        Console.WriteLine($"Integers in the range [1, 100] that are divisible by {number}:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % number == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
